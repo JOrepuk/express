cbb7cbe37996decd81b146520a27d86615ca3504
unknown <jorepuk@gmail.com>
2020-05-25 19:22:15 +0200

    admin

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://admin:Mfw5iMpV9oIEnhVR@cluster0-rh5yn.mongodb.net/test?retryWrites=true&w=majority',
+    keySession: ['projekt1'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
